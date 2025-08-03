44a606c602c03d47e43aa7288e23e8d6bd67c084
ana granada <ana2002granada@gmail.com>
2025-08-03 18:04:49 -0500

    feat: add mock service for subscription management

---
+++ src/domain/services/Subscription.service.mock.ts
@@ -0,0 +1,72 @@
+import { Subscription } from "@/domain/models/Subscription";
+import subscriptionsData from "@/domain/mocks/subscriptions.ts";
+
+const STORAGE_KEY = "mock_subscriptions";
+
+function getStoredSubscriptions(): Partial<Subscription>[] {
+    let data = localStorage.getItem(STORAGE_KEY);
+
+    if(!data) {
+        data = JSON.stringify(subscriptionsData);
+
+        localStorage.setItem(STORAGE_KEY, data);
+    }
+
+    return data ? JSON.parse(data) : [];
+}
+
+function saveSubscriptions(subscriptions: Partial<Subscription>[]) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(subscriptions));
+}
+
+async function index(): Promise<Partial<Subscription>[]> {
+    return getStoredSubscriptions();
+}
+
+async function show(id: number): Promise<Partial<Subscription> | undefined> {
+    const subscriptions = getStoredSubscriptions();
+    return subscriptions.find((s) => s.id === id);
+}
+
+async function store(data: Partial<Subscription>): Promise<Partial<Subscription>> {
+    const subscriptions = getStoredSubscriptions();
+    const newSubscription: Partial<Subscription> = {
+        ...data,
+        id: subscriptions.length + 1,
+    };
+    subscriptions.push(newSubscription);
+    saveSubscriptions(subscriptions);
+    return newSubscription;
+}
+
+async function update(subscription: Partial<Subscription>, data: Partial<Subscription>): Promise<Partial<Subscription> | null> {
+    const subscriptions = getStoredSubscriptions();
+    const index = subscriptions.findIndex((s) => s.id === subscription.id);
+
+    if (index === -1) return null;
+
+    const updated = { ...subscriptions[index], ...data };
+    subscriptions[index] = updated;
+    saveSubscriptions(subscriptions);
+
+    return updated;
+}
+
+async function remove(id: number): Promise<boolean> {
+    const subscriptions = getStoredSubscriptions();
+    const index = subscriptions.findIndex((m) => m.id === id);
+
+    if (index === -1) return false;
+
+    saveSubscriptions(subscriptions.filter((subscription) => subscription.id !== id));
+
+    return true;
+}
+
+export default {
+    index,
+    show,
+    store,
+    update,
+    remove,
+};
