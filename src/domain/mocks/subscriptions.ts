6455146bcf318bb22745bc0f746e6d9e3276e6c0
ana granada <ana2002granada@gmail.com>
2025-08-03 18:12:16 -0500

    feat: update Subscription interface and add mock subscriptions data

---
+++ src/domain/mocks/subscriptions.ts
@@ -0,0 +1,26 @@
+import {Subscription} from "@/domain/models/Subscription.ts";
+
+const subscriptions: Subscription[] = [
+    {
+        id: 1,
+        email: "bobby@gmail.com",
+    },
+    {
+        id: 2,
+        email: "Kaiya@gmail.com",
+    },
+    {
+        id: 3,
+        email: "Geidt@gmail.com",
+    },
+    {
+        id: 4,
+        email: "Abram@gmail.com",
+    },
+    {
+        id: 5,
+        email: "Carla@gmail.com",
+    },
+];
+
+export default subscriptions;
--- src/domain/models/Subscription.ts
+++ src/domain/models/Subscription.ts
@@ -1,6 +1,4 @@
 export interface Subscription {
     id: number,
-    name: string,
     email: string,
-    phone?: string,
 }
